1a81d4f755a556756311c6d7634090d803400705
mehdioui <nadjim.mehdioui@gmail.com>
2017-01-16 16:38:24 +0100

    Projet Maven

---
+++ ProjetJava/src/main/java/stri/ProjetJava/App.java
@@ -0,0 +1,13 @@
+package stri.ProjetJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!!" );
+    }
+}
